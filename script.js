72ac243b1a7feab93b87d58bbb2bf0b81f89f10f
Матмусаев Адилет Суйунбекович <a.matmusaev@terfit.local>
2023-12-04 15:19:47 +0300

    Сайт To-Dolist with local memory

---
+++ script.js
@@ -0,0 +1,41 @@
+//Импорт элементов с HTML
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+//функцию для кнопки Добавить задачу
+function addTask(){
+    if(inputBox.value === '') {
+        alert("Поле не может быть пустым!");
+    }
+    else {
+        let li = document.createElement ("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+
+}
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveData();
+    }
+    
+},false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML); 
+}
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask();
